53dbbfe8ec3cbca31fbbff0a9a0ff1514000e05a
shuhei-sugiue <shuuu1059@gmail.com>
2020-08-19 16:44:42 +0900

    プルダウンの実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,24 @@
 class ItemsController < ApplicationController
   def index
+    @items = Item.order("created_at DESC")
   end
 
   def new
     @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name,:description,:categories_id,:state_id,:charge_id,:shipper_id,:ship_date_id,:price)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,12 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category
+
+  validates :name, :description, :category, :state, :charge, :shipper, :ship_date, :price, presence: true
+
+  validates :categories_id, :state_id, :charge_id, :shipper_id, :ship_date_id, numericality: { other_than: 1 }
+
   belongs_to :user
   has_one :purchaser
+
 end
