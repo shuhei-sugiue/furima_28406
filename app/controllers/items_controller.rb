0cade305b0f0055bb6440253e1d01fc9ea4e0048
shuhei-sugiue <shuuu1059@gmail.com>
2020-08-25 16:20:16 +0900

    before_action実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,7 @@
 class ItemsController < ApplicationController
+  before_action :set_item, only: [:show]
+  
+  
   def index
     @items = Item.all.order('created_at DESC')
   end
@@ -17,12 +20,15 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   private
 
   def item_params
     params.require(:item).permit(:name, :discription, :category_id, :state_id, :charge_id, :shipper_id, :ship_date_id, :price, :image).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 end
