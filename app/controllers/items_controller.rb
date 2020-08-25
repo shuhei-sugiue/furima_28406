fcf38f4459d6d65b5cfad2120d5f791166a470f1
shuhei-sugiue <shuuu1059@gmail.com>
2020-08-25 18:57:21 +0900

    rubocup実行

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,6 @@
 class ItemsController < ApplicationController
-  before_action :set_item, only: [:show,:destroy,:edit]
-  
-  
+  before_action :set_item, only: [:show, :destroy, :edit]
+
   def index
     @items = Item.all.order('created_at DESC')
   end
