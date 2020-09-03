28ddd81bd73d773754d5b30fc627890b87999891
shuhei-sugiue <shuuu1059@gmail.com>
2020-09-03 10:32:03 +0900

    不要なコメントアウトの削除

--- app/forms/purchaser_address.rb
+++ app/forms/purchaser_address.rb
@@ -3,9 +3,12 @@ class PurchaserAddress
     include ActiveModel::Model
     attr_accessor :zip_code, :province, :municipal, :street_number, :building, :telephone, :token, :item_id, :user_id
 
+    eisuji = /\A[0-9]+\z/
+    eisujihaihun = /\A[0-9]{3}-[0-9]{4}\z/
+
     with_options presence: true do
-      validates :zip_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
-      validates :telephone, length: { maximum: 11 }, format: {with: /\A[0-9]+\z/}
+      validates :zip_code, format: {with: eisujihaihun, message: "is invalid. Include hyphen(-)"}
+      validates :telephone, length: { maximum: 11 }, format: {with: eisuji}
       validates :municipal
       validates :street_number
       validates :province, numericality: { other_than: 0, message: "can't be blank" }
